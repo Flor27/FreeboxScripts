81694417b6ffee48ace3f9f9416bb4c4e7d3ed65
Flor27 <flor27@gmail.com>
2016-06-24 14:27:44 +0200

    Create addFBXtracker.sh

---
+++ addFBXtracker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+lockfile=/var/lock/addFBXtracker
+
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null; then
+
+        trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+
+        # do stuff here
+
+/usr/local/sbin/addFBXtrackers.pl
+
+        # clean up after yourself, and release your trap
+        rm -f "$lockfile"
+        trap - INT TERM EXIT
+else
+        echo "Lock Exists: $lockfile owned by $(cat $lockfile)"
+fi
