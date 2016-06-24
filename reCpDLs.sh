e4d69f8f1b25361867109c9f35665f26ef8b2850
Flor27 <flor27@gmail.com>
2016-06-24 14:34:43 +0200

    Create reCpDLs.sh

---
+++ reCpDLs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+lockfile=/var/lock/cpDLs
+
+if ( set -o noclobber; echo "$$" > "$lockfile") 2> /dev/null; then
+
+        trap 'rm -f "$lockfile"; exit $?' INT TERM EXIT
+
+        # do stuff here
+
+/usr/local/sbin/reCpDLs.pl
+
+        # clean up after yourself, and release your trap
+        rm -f "$lockfile"
+        trap - INT TERM EXIT
+else
+        echo " Lock Exists: $lockfile owned by $(cat $lockfile) "
+fi
+
+
